745f38943fdbbb06776917434d855f6cbf6fdc84
baileyrechkemmer <rechkemmer3@gmail.com>
2023-04-29 19:10:47 -0500

    created path for admin page

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#what u addd at the admin page path
+
+class Project(models.Model):
+    title = models.CharField(max_length=250)
+    project_image = models.ImageField(upload_to='images/')
+    discription = models.TextField()
+    
