bb4b76bda20f208f5befb21460c36b7721c460c4
baileyrechkemmer <rechkemmer3@gmail.com>
2023-04-29 19:19:26 -0500

    created admin register path for projects

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project
+
 
 # Register your models here.
+# for admin page
+
+#samm as in modles.py for name and what not
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
