2e2467df0b12ee3b81eaec13665a68f874c08b58
baileyrechkemmer <rechkemmer3@gmail.com>
2023-05-01 19:21:23 -0500

    added classes stuff for admin page

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Project
-
+from .models import Classes
 
 # Register your models here.
 # for admin page
@@ -9,4 +9,8 @@ from .models import Project
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Classes)
+class ClassesAdmin(admin.ModelAdmin):
     pass
--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
-#what u addd at the admin page path
+#what u added at the admin page path
 
 class Project(models.Model):
     title = models.CharField(max_length=250)
     project_image = models.ImageField(upload_to='images/')
-    discription = models.TextField()
-    
+    description = models.TextField()
+
+class Classes(models.Model):
+    class_title = models.CharField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.DateField()
+# ask dad about price and if yes add price felid
+
+# at a later date have a felid possibly to use for the card for the travel and logins information 
