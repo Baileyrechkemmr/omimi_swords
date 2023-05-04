1c57e64865fdbf36fb7a4320458b69685c059342
baileyrechkemmer <rechkemmer3@gmail.com>
2023-05-04 18:25:07 -0500

    added sword_img to the admin veiw page

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Project
 from .models import Classes
+from .models import Sword_img
 
 # Register your models here.
 # for admin page
@@ -13,4 +14,8 @@ class ProjectAdmin(admin.ModelAdmin):
 
 @admin.register(Classes)
 class ClassesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Sword_img)
+class Sword_imgAdmin(admin.ModelAdmin):
     pass
