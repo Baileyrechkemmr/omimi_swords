43a66359a59df441cb317a643e5453263d65ce51
baileyrechkemmer <rechkemmer3@gmail.com>
2023-05-06 11:19:06 -0500

    added template to home page

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from .models import Project, Classes
 # Create your views here.
 def home(request):
-    return render(request, 'projects/home.html')
+    projects = Project.objects
+    return render(request, 'projects/home.html', {'projects': projects})
 
 def about(request):
     return render(request, 'projects/about.html')
 
 def classes(request):
-    return render(request, 'projects/classes.html')
+    classes = Classes.objects
+    return render(request, 'projects/classes.html', {'classes': classes})
