ed74075a372aa858850e11cb7c5b6ba8aed9ee56
baileyrechkemmer <rechkemmer3@gmail.com>
2023-05-06 11:45:13 -0500

    added search feild

--- projects/admin.py
+++ projects/admin.py
@@ -11,11 +11,14 @@ from .models import Sword_img
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['title']
+    search_fields = ['title', 'description']
 
 @admin.register(Classes)
 class ClassesAdmin(admin.ModelAdmin):
     list_display = ['class_title']
+    search_fields = ['class_title']
 
 @admin.register(Sword_img)
 class Sword_imgAdmin(admin.ModelAdmin):
     list_display = ['title']
+    search_fields = ['title']
