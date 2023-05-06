1e3b0fe79ecb801db7de61a9f8e8679b2d4b966e
baileyrechkemmer <rechkemmer3@gmail.com>
2023-05-06 11:40:54 -0500

    titles for everything are now added to admin screan

--- projects/models.py
+++ projects/models.py
@@ -7,16 +7,25 @@ class Project(models.Model):
     title = models.CharField(max_length=250)
     project_image = models.ImageField(upload_to='images/')
     description = models.TextField()
+    
+    def __str__(self):
+        return self.title
 
 class Classes(models.Model):
     class_title = models.CharField(max_length=250)
     start_date = models.DateField()
     end_date = models.DateField()
+
+    def __str__(self):
+        return self.class_title
 # ask dad about price and if yes add price felid
 
 class Sword_img(models.Model):
     title = models.CharField(max_length=250)
     image = models.ImageField(upload_to='images/')
+    
+    def __str__(self):
+        return self.title
 
 
 # at a later date have a felid possibly to use for the card for the travel and logins information 
