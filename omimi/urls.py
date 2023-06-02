f64f828d9a37c27887e0f3e6ae2cd1e7b9c3a505
baileyrechkemmer <rechkemmer3@gmail.com>
2023-06-02 14:09:17 -0500

    hotel details are on the page

--- omimi/urls.py
+++ omimi/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('gallery/', projects.views.gallery, name='gallery'),
     path('details_s/<int:sword_img_id>',projects.views.details_s, name='details_s'),
     path('order_form/', projects.views.order_form, name='order_form'),
-    path('details_h/<int:hotels_id>', projects.views.details_h, name='details_h'),
+    path('details_h/<int:hotel_id>', projects.views.details_h, name='details_h'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
