b5fdbce547aa504b8fa1ef5b5d25bb7e4bb4136e
baileyrechkemmer <rechkemmer3@gmail.com>
2023-07-01 15:49:50 -0500

    added forms for sales form

--- projects/forms.py
+++ projects/forms.py
@@ -32,3 +32,15 @@ class OrderForm(forms.Form):
     type_of_steel = forms.ChoiceField(choices=STEEL_CHOICE)
     length_of_blade = forms.IntegerField(max_length=32)
     other_specifications = forms.ChoiceField(widget=forms.Textarea)
+
+class SalesForm(forms.Form):
+    Item_Number = forms.IntegerField()
+    email = forms.EmailField()
+    name = forms.CharField(max_length=100)
+    address_1 = forms.CharField(max_length=100)
+    address_2 = forms.CharField(max_length=100)
+    city = forms.CharField(max_length=100)
+    state_or_province = forms.CharField(max_length=100)
+    zip_code = forms.CharField(max_length=100)
+    country = forms.CharField(max_length=100)
+    phone_number = forms.CharField(max_length=13)
