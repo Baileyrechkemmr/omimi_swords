13fa2b9efe8f343cd5bc7372c1562a85654d2032
baileyrechkemmer <rechkemmer3@gmail.com>
2023-07-06 16:42:25 -0500

    order form finished

--- omimi/settings.py
+++ omimi/settings.py
@@ -28,8 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # Use 465 for SSL/TLS encryption
+EMAIL_USE_TLS = True  # Use False for SSL/TLS encryption
+EMAIL_HOST_USER = ''  # Sender's email address
+EMAIL_HOST_PASSWORD = ''
 
 # Application definition
 
