90eae7577f516319aaf0b1dad6b199772f4dcf77
baileyrechkemmer <rechkemmer3@gmail.com>
2023-11-22 14:38:07 -0600

    deployment on render

--- omimi/settings.py
+++ omimi/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'omimi.wsgi.application'
 
 # local host
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 #  live render
 
